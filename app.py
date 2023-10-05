bf58bc5ee46cf8a730463f8cd561315fc5d2040a
NeelBlox <76996706+NeelBlox@users.noreply.github.com>
2023-10-05 14:20:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import  Flask, request, redirect, url_for, render_template, jsonify
 import json
 import requests
+import string
+import secrets
 from firebase import firebase
 firebase = firebase.FirebaseApplication("https://premis-f8e8d-default-rtdb.firebaseio.com/", None)
 app = Flask(__name__)
@@ -11,7 +13,7 @@ def no():
 @app.route('/premis/<string:n>')
 def premis(n):
     user_id = firebase.get('/',n)
-    if user_id!=None:
+    if user_id==True:
         result={
         "access_key":n,
         "user_id":user_id,
@@ -22,6 +24,21 @@ def premis(n):
             "status":False
             }
     return jsonify(result)
-    
+@app.route('/premis/code=<string:m>')
+def gen(m):
+    if m=='parthmani':
+        characters = string.ascii_lowercase + string.digits
+        key = ''.join(secrets.choice(characters) for _ in range(16))
+        np=True
+        result={
+            "key":key,
+            "status":np
+            }
+        firebase.put('/',key,np)
+    else:
+        result={
+            "key":'Code invalid not authorized'
+            }
+    return result
 if __name__=="__main__":
     app.run()
