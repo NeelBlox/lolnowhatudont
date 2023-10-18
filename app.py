281e1d063890acc4bb0bcf02caba6cb9c11d2768
NeelBlox <76996706+NeelBlox@users.noreply.github.com>
2023-10-18 18:15:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ from flask import  Flask, request, redirect, url_for, render_template, jsonify
 import json
 import requests
 import string
+
 import secrets
 from firebase import firebase
-firebase = firebase.FirebaseApplication("https://premis-f8e8d-default-rtdb.firebaseio.com/", None)
+firebase1 = firebase.FirebaseApplication("https://premis-f8e8d-default-rtdb.firebaseio.com/", None)
+firebase2=firebase.FirebaseApplication("https://premis-userdb-default-rtdb.firebaseio.com/")
 app = Flask(__name__)
 @app.route('/')
 def no():
-    return '?'
+    moh={
+        "message":"Premis API is live",
+        "success": True
+        }
+    return moh
 
 @app.route('/premis/<string:n>')
 def premis(n):
-    user_id = firebase.get('/',n)
+    user_id = firebase1.get('/',n)
     if user_id==True:
         result={
         "access_key":n,
@@ -24,21 +30,33 @@ def premis(n):
             "status":False
             }
     return jsonify(result)
-@app.route('/premis/code=<string:m>')
-def gen(m):
-    if m=='parthmani':
-        characters = string.ascii_lowercase + string.digits
-        key = ''.join(secrets.choice(characters) for _ in range(16))
-        np=True
+@app.route('/premis/code=<string:m>/user=<int:y>')
+def gen(m,y):
+    get_first=firebase2.get('/',y)
+    
+    if get_first==True:
         result={
-            "key":key,
-            "status":np
+            "status":False,
+            "reason":"User already has a key!"
             }
-        firebase.put('/',key,np)
+        
     else:
-        result={
-            "status":False
+        if m=='parthmani':
+            characters = string.ascii_lowercase + string.digits
+            key = ''.join(secrets.choice(characters) for _ in range(16))
+            np=True
+            result={
+                "key":key,
+                "status":True
+                }
+            firebase1.put('/',key,np)
+            firebase2.put('/',y,np)
+        else:
+            result={
+            "status":"notok"
             }
     return result
+
+
 if __name__=="__main__":
     app.run()
